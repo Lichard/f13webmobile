87a43cb821178038047f51358d6df4b3c977e876
Lichard <ryli721@gmail.com>
2013-09-18 20:26:55 -0400

    finished php

--- hw2/hw2.php
+++ hw2/hw2.php
@@ -1,8 +1,12 @@
 <?php
     $time = $_GET['time'];
     $name = $_GET['name'];
+    $result = new DateTime($time);
+    $initial = new DateTime($time);
+    $result->sub( new DateInterval('PT8H'));
 
-    echo 'Hello ' . $name;
-    echo "To wake up at" .  $time . "you should try to sleep at ";
+    echo 'Hello ' . $name . '!<br>';
+    echo "To wake up at " . date_format($initial, 'h:i A') .  " you should try to sleep by " . date_format($result, 'h:i A') . ".";
+
     
 ?>
