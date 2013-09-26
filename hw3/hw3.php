9f2bd7c3976cfb626228f8026c04e2c7828f822a
Richard Li <ryli721@gmail.com>
2013-09-25 21:58:36 -0400

    added insert sql

--- hw3/hw3.php
+++ hw3/hw3.php
@@ -12,9 +12,17 @@ if($type!=null){
         $name = $_GET['name'];
         $comment = $_GET['comment'];
         $time = time();
-        
 
+        $stmt = $db_connection->stmt_init();
+        $numRecords = 0;
+        if($stmt->prepare("insert into Comments values(?,?,?)")){
 
+            $stmt->bind_param("sss", $time, $name, $comment);
+            $stmt->execute();
+
+            $stmt->close();
+        }
+        $db_connection->close();
 
         echo 'Comment posted! '  . $name . " commented: '" . $comment;
     }
@@ -22,6 +30,18 @@ if($type!=null){
         $name = $_GET['updateName'];
         $age = $_GET['updateAge'];
         $info = $_GET['info'];
+        $stmt->bind_result($date, $day, $num, $topic, $desc);
+
+        while ($stmt->fetch()) {
+            $numRecords = $numRecords + 1;
+            echo "Lecture: " . $num . "<br/>";
+            echo "Topic: " . $topic . "<br/>";
+            echo "Description: " . $desc . "<br/>";
+        }
+
+        if($numRecords == 0) {
+            echo "No lecutre today.<br/>";
+        }
         echo 'Successfully updated info for' . $name . "!";
     }
     if($type=='viewCommentForm'){
