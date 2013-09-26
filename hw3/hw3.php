bd6dc77ef226da6671254bb7edfa8333faccc524
Lichard <ryli721@gmail.com>
2013-09-25 23:13:14 -0400

    added all 3 features

--- hw3/hw3.php
+++ hw3/hw3.php
@@ -15,38 +15,73 @@ if($type!=null){
 
         $stmt = $db_connection->stmt_init();
         $numRecords = 0;
-        if($stmt->prepare("insert into Comments values(?,?,?)")){
-
-            $stmt->bind_param("sss", $time, $name, $comment);
+        if($stmt->prepare("insert into Comments (`User`, `Message`) values(?,?)")){
+            $stmt->bind_param("ss",$name, $comment);
             $stmt->execute();
-
             $stmt->close();
         }
-        $db_connection->close();
 
+        $stm = $db_connection->stmt_init();
+        $numRecords = 0;
+        if($stm->prepare("insert into Users (`Name`) values(?)")){
+            $stm->bind_param("s",$name);
+            $stm->execute();
+            $stm->close();
+        }
+        $db_connection->close();
         echo 'Comment posted! '  . $name . " commented: '" . $comment;
     }
     if($type=='userForm'){
         $name = $_GET['updateName'];
         $age = $_GET['updateAge'];
-        $info = $_GET['info'];
-        $stmt->bind_result($date, $day, $num, $topic, $desc);
+        $info = $_GET['updateInfo'];
 
-        while ($stmt->fetch()) {
-            $numRecords = $numRecords + 1;
-            echo "Lecture: " . $num . "<br/>";
-            echo "Topic: " . $topic . "<br/>";
-            echo "Description: " . $desc . "<br/>";
-        }
+        $stmt = $db_connection->stmt_init();
+        $numRecords = 0;
+        if($stmt->prepare("update Users set `age` = ?, `info`=? where `name` = ?")){
 
-        if($numRecords == 0) {
-            echo "No lecutre today.<br/>";
+            $stmt->bind_param("sss", $age, $info, $name);
+            $stmt->execute();
+            $stmt->bind_result($date, $day, $num, $topic, $desc);
+
+            while ($stmt->fetch()) {
+                $numRecords = $numRecords + 1;
+                echo "Lecture: " . $num . "<br/>";
+                echo "Topic: " . $topic . "<br/>";
+                echo "Description: " . $desc . "<br/>";
+            }
+
+            if($numRecords == 0) {
+                echo "No lecutre today.<br/>";
+            }
+            $stmt->close();
         }
+        $db_connection->close();
         echo 'Successfully updated info for' . $name . "!";
     }
     if($type=='viewCommentForm'){
+        $stmt = $db_connection->stmt_init();
+        $numRecords = 0;
+        if($stmt->prepare("select Comments.Timestamp, Comments.message, Users.name, Users.age, Users.info from Comments inner join Users on Comments.user=Users.name")){
+            $stmt->execute();
+            $stmt->bind_result($timestamp, $message, $name, $age, $info);
 
-        echo $name . " " . $comment . " " . $time;
+            while ($stmt->fetch()) {
+                $numRecords = $numRecords + 1;
+                echo "Timestamp: " . $timestamp . "<br/>";
+                echo "Comment: " . $message . "<br/>";
+                echo "By User: " . $name . " ";
+                echo "Age: " . $age . " ";
+                echo "Info: " . $info . "<br/>";
+                echo "<br/>";
+            }
+
+            if($numRecords == 0) {
+                echo "No comments yet.<br/>";
+            }
+            $stmt->close();
+        }
+        $db_connection->close();
     }
 }
 ?>
